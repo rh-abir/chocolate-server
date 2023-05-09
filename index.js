c1e583fbebdafd533c4e4f9beb7af3d53e929004
rh-abir <rhabir.co@gmail.com>
2023-05-09 18:42:34 +0600

    add delete chocolate

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
-// middleware 
-app.use(cors())
-app.use(express.json())
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // chocolateMaster
 // IGUSzNXm2EvRn4LU
 
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.as0kbdg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,16 +23,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const chocolateCollection = client
+      .db("chocolatesDB")
+      .collection("chocolate");
+
+    app.get("/allChocolates", async (req, res) => {
+      const cursor = await chocolateCollection.find().toArray();
+      res.send(cursor);
+    });
+
+    app.post("/chocolates", async (req, res) => {
+      const body = req.body;
+      const result = await chocolateCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.delete("/chocolates/:id", async(req, res) => {
+      const id = req.params.id;
+      // console.log("hitting delete", id);
+      const qurey = { _id: new ObjectId(id) };
+      const result = await chocolateCollection.deleteOne(qurey);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,14 +66,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('chocolate server is running')
-})
-
+app.get("/", (req, res) => {
+  res.send("chocolate server is running");
+});
 
 app.listen(port, () => {
-    console.log(`chocolate server running on ${port}`)
-})
+  console.log(`chocolate server running on ${port}`);
+});
